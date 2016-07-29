3e2de7cd0c5ee50442b7a22920922936968537d5
othmane <othmane.abouelyzza@gmail.com>
2016-07-30 01:10:56 +0200

    Adding inheritance basic example

---
+++ src/inheritance/Man.java
@@ -0,0 +1,7 @@
+package inheritance;
+
+public class Man extends Human {
+    public Man(String firstName, String lastName, int age) {
+        super(firstName, lastName, age);
+    }
+}
---
+++ src/inheritance/Woman.java
@@ -0,0 +1,7 @@
+package inheritance;
+
+public class Woman extends Human {
+    public Woman(String firstName, String lastName, int age) {
+        super(firstName, lastName, age);
+    }
+}
